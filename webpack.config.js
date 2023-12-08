81505076b19fadc1c68b4dab35d6ef104e815232
Adam Urbanik <adam.urbanik@sinch.com>
2023-12-08 16:50:27 +0100

    multiple bundles, loaders, plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,29 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 let production = process.env.NODE_ENV === 'production';
 
 let config = {
-    entry: ['./src/index', './src/home'],   // it is looking for these files and if it does not find .ts files it will look for .js file
+    // it is looking for these files and if it does not find .ts files it will look for .js file
+    // multiple bundles - each entry will have output filename
+    entry: {
+        index1: './src/index',
+        home1: './src/home'
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].js', // it will take the name of the entry point i.e. index. home and put it in the file.js
         path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
+            // ts loader - take typescript and change it to javascript
             {
                 test: /\.ts$/,
                 exclude: /node_modules/,
                 use: 'ts-loader'
             }
+            // babel loader - take modern es6 js file and change it to es5
+            //
             // {
             //     test: /\.js$/,
             //     exclude: /node_modules/,
@@ -28,6 +36,10 @@ let config = {
             // }
         ]
     },
+    plugins: [
+        // plugin to produce index.html file. template option is using our own index.html and injects all the bundle files in produced html file
+        new HtmlWebpackPlugin({ template: "./index.html" })
+    ],
     resolve: {
         extensions: ['.ts', '.js']   // search for .ts file, if not search for .js file. We tell webpack that we work with ts files, not only with js files.
     },
